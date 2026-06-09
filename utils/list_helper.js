0b004c8dfc32909f339fd9b8ca9479e7b211a79f
M-hub-7 <mfendemnguidje@gmail.com>
2026-06-09 18:47:17 -0500

    Exercices 4.3.-4.7  Fonctions utilitaires et tests unitaires, étape 2

---
+++ utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+  if (Array.isArray(blogs)) {
+    return 1
+  }
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+module.exports = {
+  dummy,
+  totalLikes,
+}
