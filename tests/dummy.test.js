9b3c12593cc4693ffc952fb3268e462afdadb219
M-hub-7 <mfendemnguidje@gmail.com>
2026-06-09 19:23:58 -0500

    Exercices 4.3.-4.7 4.5* : Fonctions utilitaires et tests unitaires, étape 3

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -7,6 +7,7 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
 describe('total likes', () => {
   const listWithOneBlog = [
     {
@@ -20,5 +21,35 @@ describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
+  })
+})
+
+describe('favorites', () => {
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    },
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'la vie d une developpeuse',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 10,
+    },
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'la vie d une developpeuse',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 10,
+    },
+  ]
+  test('the faborites blog is ', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[2])
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,26 @@
-const dummy = (blogs) => {
-  if (Array.isArray(blogs)) {
+const dummy = (blog) => {
+  if (Array.isArray(blog)) {
     return 1
   }
 }
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => {
-    return sum + blog.likes
+const totalLikes = (blog) => {
+  return blog.reduce((sum, blogs) => {
+    return sum + blogs.likes
   }, 0)
+}
+
+const favoriteBlog = (blog) => {
+  let favori = blog[0]
+  blog.forEach((blogs) => {
+    if (blogs.likes > favori.likes) {
+      favori = blogs
+    }
+  })
+  return favori
 }
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
