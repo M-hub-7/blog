f0fdf5dc4bc2cf62890827d7baa39187395e6fda
M-hub-7 <mfendemnguidje@gmail.com>
2026-06-11 15:41:44 -0500

    Exercices 4.8.-4.12 4.12* : Tests de liste de blogs, étape 5

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const app = require('../app')
 const Blog = require('../models/blog')
 const assert = require('node:assert')
+
 const api = supertest(app)
 
 const initialBlog = [
@@ -23,7 +24,7 @@ const initialBlog = [
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  await Blog.insertMany(initialBlog)
+  await Promise.all(initialBlog.map((blog) => new Blog(blog).save()))
 })
 
 test('note are retourned as json', async () => {
@@ -54,10 +55,29 @@ test('add new blog', async () => {
     .send(newblogs)
     .expect(201)
     .expect('Content-Type', /application\/json/)
+
   const response = await api.get('/api/blogs')
+
   const title = response.body.map((e) => e.title)
   assert(title.includes('bienvenu sur mon nouveau blog'))
   assert.strictEqual(response.body.length, initialBlog.length + 1)
+})
+
+test('new block without url and title', async () => {
+  const newblogs = {
+    author: 'mfendem daina',
+    likes: 1000,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newblogs)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlog.length)
 })
 
 test('verification of like proprieties', async () => {
@@ -67,7 +87,8 @@ test('verification of like proprieties', async () => {
     url: 'http://mfendemdaina.com',
   }
   const response = await api.post('/api/blogs').send(newblogs).expect(201)
-  assert.strictEqual(response.body.likes, undefined)
+
+  assert.strictEqual(response.body.likes, 0)
 })
 
 after(async () => {
